b4bf85ddc4bea8824f36ac530eab4e8d581ce5a7
BiliDownloader <3112117950@qq.com>
2026-09-18 13:06:46 +0800

    Update repo name to streaming-video-downloader

--- gui/views/about_view.py
+++ gui/views/about_view.py
@@ -3,7 +3,7 @@ import customtkinter as ctk
 
 from gui.theme import COLORS, FONTS
 
-GITHUB_URL = "https://github.com/arcadia-cloud/bili-downloader"
+GITHUB_URL = "https://github.com/arcadia-cloud/streaming-video-downloader"
 
 
 class AboutView(ctk.CTkFrame):
